a43d2e33b76aa75c9e867ae9b19d55374d653692
Ted Kaemming <ted@kaemming.com>
2015-12-02 23:23:08 -0800

    allow writer to start sequence at arbitrary interval

--- writer.py
+++ writer.py
@@ -1,3 +1,4 @@
+import itertools
 import sys
 from redis.client import (
     Script,
@@ -9,5 +10,6 @@ client = StrictRedis()
 push = client.register_script(open('push.lua').read())
 
 n = int(sys.argv[1])
+s = itertools.count(int(sys.argv[2]))
 for x in xrange(0, n):
-    print push(('topic',), (1024, 60 * 60 * 24 * 7, x))
+    print push(('topic',), (1024, 60 * 60 * 24 * 7, next(s)))
