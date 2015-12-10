314d818292e4eb1eeb507f3a929c0016c5832e9d
Ted Kaemming <ted@kaemming.com>
2015-12-09 16:57:56 -0800

    use hash for topic config

--- kafkaesque/scripts/create.lua
+++ kafkaesque/scripts/create.lua
@@ -1,11 +1,19 @@
 local topic = KEYS[1]
 
-local size = tonumber(ARGV[1])
-local ttl = tonumber(ARGV[2])
+assert(redis.call('EXISTS', topic) == 0)
 
 local configuration = {
-    size=size,
-    ttl=ttl
+    size=tonumber(ARGV[1]),
+    ttl=tonumber(ARGV[2]),
 }
 
-assert(redis.call('SETNX', topic, cmsgpack.pack(configuration)) == 1, 'topic already exists')
+-- TODO: There has to be a more elegant way to do this.
+local options = {}
+for k, v in pairs(configuration) do
+    if v ~= nil then
+        table.insert(options, k)
+        table.insert(options, v)
+    end
+end
+
+redis.call('HMSET', topic, unpack(options))
--- kafkaesque/scripts/push.lua
+++ kafkaesque/scripts/push.lua
@@ -2,20 +2,26 @@ local topic = KEYS[1]
 
 local items = ARGV
 
-local configuration = redis.call('GET', topic)
-assert(configuration, 'topic does not exist')
-configuration = cmsgpack.unpack(configuration)
+local function load_configuration()
+    local values = redis.call('HMGET', topic, 'size', 'ttl')
+    return {
+        size=tonumber(values[1]),
+        ttl=tonumber(values[2])
+    }
+end
+
+local configuration = load_configuration()
 
 local number = 0
 local offset = 0
 local length = 0  -- length of current page
 
-local function start_page ()
+local function start_page()
     redis.log(redis.LOG_DEBUG, string.format('Starting new page %s#%s.', topic, number))
     assert(redis.call('ZADD', topic .. '/pages', offset, number) == 1)
 end
 
-local function close_page ()
+local function close_page()
     if configuration['ttl'] ~= nil then
         redis.call('EXPIRE', topic .. '/pages/' .. number, configuration['ttl'])
         redis.log(redis.LOG_DEBUG, string.format('Set %s#%s to expire in %s seconds.', topic, number, configuration['ttl']))
@@ -24,7 +30,7 @@ local function close_page ()
     length = 0
 end
 
-local function check_page ()
+local function check_page()
     if length >= configuration['size'] then
         close_page()
         start_page()
