591eef9e4a795ac340bff77721ee115edbbd8dae
Ted Kaemming <ted@kaemming.com>
2015-12-09 16:20:29 -0800

    fix resources

--- kafkaesque/topic.py
+++ kafkaesque/topic.py
@@ -1,11 +1,18 @@
+import functools
+from pkg_resources import resource_string
+
+
+resource_string = functools.partial(resource_string,  'kafkaesque')
+
+
 class Topic(object):
     def __init__(self, client, topic):
         self.client = client
         self.topic = topic
 
-        self.__create = client.register_script(open('scripts/create.lua').read())
-        self.__push = client.register_script(open('scripts/push.lua').read())
-        self.__pull = client.register_script(open('scripts/pull.lua').read())
+        self.__create = client.register_script(resource_string('scripts/create.lua'))
+        self.__push = client.register_script(resource_string('scripts/push.lua'))
+        self.__pull = client.register_script(resource_string('scripts/pull.lua'))
 
     def create(self, size=1024, ttl=None):
         return self.__create((self.topic,), (size, ttl))
